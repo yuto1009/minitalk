7e767ef826ce2599e193bbb45b69d011d2038338
Yuto Endo <yuendo@c1r12s7.42tokyo.jp>
2023-10-07 17:50:37 +0900

    uploading 2023 oct7th 42

--- client.c
+++ client.c
@@ -6,44 +6,44 @@
 /*   By: yuendo <yuendo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 12:55:56 by yuendo            #+#    #+#             */
-/*   Updated: 2023/10/07 16:35:24 by yuendo           ###   ########.fr       */
+/*   Updated: 2023/10/07 17:49:22 by yuendo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void send_character(int pid, char c)
+void	send_character(int pid, char c)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < 8)
-    {
-        if ((c >> i) & 1)
-            kill(pid, SIGUSR2);
-        else
-            kill(pid, SIGUSR1);
-        i++;
-        usleep(200);
-    }
+	i = 0;
+	while (i < 8)
+	{
+		if ((c >> i) & 1)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		i++;
+		usleep(200);
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    if (argc != 3)
-    {
-        ft_printf("Usage: ./client [SERVER_PID] [STRING]\n");
-        exit(1);
-    }
+	int		pid;
+	char	*message;
 
-    int pid = atoi(argv[1]);
-    char *message = argv[2];
-
-    while (*message)
-    {
-        send_character(pid, *message);
-        message++;
-    }
-
-    return (0);
+	if (argc != 3)
+	{
+		ft_printf("Usage: ./client [SERVER_PID] [STRING]\n");
+		exit(1);
+	}
+	pid = atoi(argv[1]);
+	message = argv[2];
+	while (*message)
+	{
+		send_character(pid, *message);
+		message++;
+	}
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,48 +6,47 @@
 /*   By: yuendo <yuendo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 12:55:59 by yuendo            #+#    #+#             */
-/*   Updated: 2023/10/07 17:40:51 by yuendo           ###   ########.fr       */
+/*   Updated: 2023/10/07 17:49:08 by yuendo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void signal_handler(int signal)
+void	signal_handler(int signal)
 {
-    static char character;
-    static int bits_received;
-    
-    if (signal == SIGUSR1)
-        character |= (0 << bits_received);
-    else if (signal == SIGUSR2)
-        character |= (1 << bits_received);
-    bits_received++;
+	static char	character;
+	static int	bits_received;
 
-    if (bits_received == 8)
-    {
-        write(1, &character, 1);
-        character = 0;
-        bits_received = 0;
-    }
+	if (signal == SIGUSR1)
+		character |= (0 << bits_received);
+	else if (signal == SIGUSR2)
+		character |= (1 << bits_received);
+	bits_received++;
+	if (bits_received == 8)
+	{
+		write(1, &character, 1);
+		character = 0;
+		bits_received = 0;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    struct sigaction sa1;
-    struct sigaction sa2;
-    
-    sa1.sa_handler = signal_handler;
-    sa2.sa_handler = signal_handler;
-    sigemptyset(&sa1.sa_mask);
-    sigemptyset(&sa2.sa_mask);
-    sa1.sa_flags =  0;
-    sa2.sa_flags =  0;
-    // signal(SIGUSR1, signal_handler);
-    // signal(SIGUSR2, signal_handler);
-    sigaction(SIGUSR1, &sa1, NULL);
-    sigaction(SIGUSR2, &sa2, NULL);
-    ft_printf("Server PID: %d\n", getpid());
-    while (1)
-        pause();
-    return (0);
+	struct sigaction sa1;
+	struct sigaction sa2;
+
+	sa1.sa_handler = signal_handler;
+	sa2.sa_handler = signal_handler;
+	sigemptyset(&sa1.sa_mask);
+	sigemptyset(&sa2.sa_mask);
+	sa1.sa_flags = 0;
+	sa2.sa_flags = 0;
+	// signal(SIGUSR1, signal_handler);
+	// signal(SIGUSR2, signal_handler);
+	sigaction(SIGUSR1, &sa1, NULL);
+	sigaction(SIGUSR2, &sa2, NULL);
+	ft_printf("Server PID: %d\n", getpid());
+	while (1)
+		pause();
+	return (0);
 }
