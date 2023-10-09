8d7b8fa630253eb4c72b3dcbdb56c270417a4ca9
yuto1009 <bmwb71009@gmail.com>
2023-10-09 22:05:21 +0900

    uploading 2023 oct9th home

--- client.c
+++ client.c
@@ -6,19 +6,18 @@
 /*   By: yutoendo <yutoendo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 12:55:56 by yuendo            #+#    #+#             */
-/*   Updated: 2023/10/09 20:44:40 by yutoendo         ###   ########.fr       */
+/*   Updated: 2023/10/09 22:04:00 by yutoendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-volatile sig_atomic_t ack_received = RECEIVED;
+volatile sig_atomic_t ack_received = NOT_RECEIVED;
 
 static void    send_character(int pid, char c)
 {
     int    i;
 
-    // ack_received = NOT_RECEIVED;
     i = 0;
     while (i < 8 && ack_received == NOT_RECEIVED)
     {
@@ -28,20 +27,16 @@ static void    send_character(int pid, char c)
             kill(pid, SIGUSR1);
         i++;
         usleep(200);
-        // alarm(5);
-        // while (ack_received == NOT_RECEIVED)
-        // pause();
-        // alarm(RESET);
-        // ack_received = NOT_RECEIVED;
     }
-    usleep(500);
+    alarm(5);
+    usleep(300);
+    alarm(RESET);
 }
 
 void ack_handler(int signal)
 {
     (void)signal;
     ack_received = RECEIVED;
-    // ft_printf("aaa\n");
 }
 
 void timeout_handler(int signal)
@@ -51,27 +46,32 @@ void timeout_handler(int signal)
     exit(EXIT_FAILURE);
 }
 
+void input_error_exit()
+{
+    ft_printf("Usage: ./client [SERVER_PID] [STRING]\n");
+    exit(1);
+}
+
 int    main(int argc, char **argv)
 {
-    int        server_pid;
     char    *message;
     struct sigaction sa;
+    struct sigaction sa_alarm;
 
     if (argc != 3)
-    {
-        ft_printf("Usage: ./client [SERVER_PID] [STRING]\n");
-        exit(1);
-    }
+        input_error_exit();
     sa.sa_handler = ack_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
     sigaction(SIGUSR1, &sa, NULL);
-
-    server_pid = ft_atoi(argv[1]);
+    sa_alarm.sa_handler = timeout_handler;
+    sigemptyset(&sa_alarm.sa_mask);
+    sa_alarm.sa_flags = 0;
+    sigaction(SIGALRM, &sa_alarm, NULL);
     message = argv[2];
     while (*message)
     {
-        send_character(server_pid, *message);
+        send_character(ft_atoi(argv[1]), *message);
         ack_received = NOT_RECEIVED;
         message++;
     }
